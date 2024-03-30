b1cdbe9240f835ef330d7c0a072d265aacdbb73c
liahrothstein <liahrothstein@gmail.com>
2024-03-30 17:24:28 +0300

    refactor: add gh pages package

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/gord.onfit.random.card/",
   plugins: [react()],
   server: {
     host: true,
